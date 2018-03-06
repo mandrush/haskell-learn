57716202ea390be15ab7a58946d8f69fdb64885a
Michal Andruszkiewicz <mandruszkiewicz@wisenet.pl>
2018-03-06 12:28:51 +0100

    recursive max for lists

---
+++ recMax.hs
@@ -0,0 +1,5 @@
+-- recursive max function (determines the maximum element of a list)
+
+recMax xs = case xs of [] -> error "There is no max of an empty list"
+                       [x] -> x
+                       (x:xs) -> max x (recMax xs)
