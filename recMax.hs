b83d39bdcc7b1f3f218e9e6d3f15b83ca9f814dc
Michal Andruszkiewicz <mandruszkiewicz@wisenet.pl>
2018-03-06 12:29:55 +0100

    recmax

--- recMax.hs
+++ recMax.hs
@@ -1,5 +1,5 @@
 -- recursive max function (determines the maximum element of a list)
-
+recMax :: Ord a => [a] -> a
 recMax xs = case xs of [] -> error "There is no max of an empty list"
                        [x] -> x
                        (x:xs) -> max x (recMax xs)
